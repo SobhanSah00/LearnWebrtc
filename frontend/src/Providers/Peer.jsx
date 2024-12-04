f7d8f440550bcb2ceff5375609dd599f12dcd55f
sobhan9012 <sobhansahoo2000@gmail.com>
2024-12-04 19:52:56 +0530

    completye

--- frontend/src/Providers/Peer.jsx
+++ frontend/src/Providers/Peer.jsx
@@ -1,10 +1,13 @@
-import  { createContext,useContext,useMemo } from 'react'
+import  { createContext,useCallback,useContext,useEffect,useMemo, useState } from 'react'
+import { useSocket } from './Socket';
 
 const PeerContext = createContext(null);
 
 export const usePeer = () => useContext(PeerContext)
 
 export const PeerProvider = (props) => {
+
+    const [remoteStream,setRemoteStream] = useState(null)
 
     const { children } = props;
     const peer = useMemo(() => new RTCPeerConnection({
@@ -38,9 +41,34 @@ export const PeerProvider = (props) => {
       await peer.setRemoteDescription(ans)  
     }
 
+    const sendStream = async(stream) => {
+        const tracks = stream.getTracks();
+        // tracks.forEach(track => peer.addTrack(track, stream))
+        for(const track of tracks) {
+            peer.addTrack(track, stream)
+        }
+    }
+
+    const handelTrackEvent = useCallback((e) => {
+        const streams = e.streams; // video audio sharing screen type streams
+        setRemoteStream(streams[0])
+    }, [setRemoteStream])
+
+    // const handelNegotiation = useCallback(() => {
+    //     console.log("negotition needed");
+    // }, [])
+
+    useEffect(() => {
+        peer.addEventListener('track',handelTrackEvent)
+        // peer.addEventListener('negotiationneeded', handelNegotiation)
+        return () => {
+            peer.removeEventListener('track',handelTrackEvent)
+            // peer.removeEventListener('negotiationneeded',handelNegotiation)
+        }
+    },[peer,handelTrackEvent])
     
     return (
-        <PeerContext.Provider value={{peer,createOffer,createAnswer,setRemoteAns}}>
+        <PeerContext.Provider value={{peer,createOffer,createAnswer,setRemoteAns,sendStream,remoteStream}}>
             {children}
         </PeerContext.Provider>
     )
--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,154 +1,94 @@
-// import { useEffect,useCallback, useState } from 'react'
-// import {useSocket} from "../../Providers/Socket"
-// import { usePeer } from "../../Providers/Peer"
-// import ReactPlayer from 'react-player';
-
-// function Room() {
-//     const {socket} = useSocket();
-//     const {peer,createOffer,createAnswer,setRemoteAns} = usePeer()
-
-
-//     const [myStream,setMystream] = useState(null)
-//     const newUserJoinedAfterOneUserJoined = useCallback(async (data) => {
-//         // console.log("recived data : ",JSON.stringify(data, null, 2))
-//         const {email} = data
-//         console.log("new user joined",email);   
-//         const offer = await createOffer()
-//         socket.emit('offerForCall-User',{email,offer})
-//     },[createOffer,socket])
-
-//     const handelIncommingCall = useCallback( async (data) => {
-//       const {from,offer} = data;
-//       console.log("incomming call from",from,offer)
-//       const ans = await createAnswer(offer)
-//       socket.emit('call-accepted',{email : from,ans})
-
-//       },[createAnswer,socket])
-
-//     const handelCallAccepted = useCallback(async (data) => {
-//       const {ans} = data;
-//       console.log("call got accepted",ans)
-//       await setRemoteAns(ans)
-
-//     } ,[setRemoteAns])
-
-//     const getUserMediaStream = useCallback(async() => {
-//       const stream = await navigator.mediaDevices.getUserMedia({
-//         video: true,
-//         audio: true
-//       })
-//       setMystream(stream)
-//     }, [])
-
-//     useEffect(() => {
-//       socket.on('user-joined', newUserJoinedAfterOneUserJoined)
-//       socket.on('incomming-call',handelIncommingCall)
-//       socket.on('call-accepted',handelCallAccepted);
-//       return () => {
-//         socket.off('user-joined', newUserJoinedAfterOneUserJoined) //securitu purpose and clean up also 
-//         socket.off('incomming-call',handelIncommingCall)
-//         socket.off('call-accepted',handelCallAccepted);
-//       }
-//     }, [socket,newUserJoinedAfterOneUserJoined,handelIncommingCall,handelCallAccepted])
-    
-//     useEffect(() => {
-//       getUserMediaStream()
-//     },[getUserMediaStream])
-
-//   return (
-//     <>
-//         <div className='Room-page-container'>
-//             <h1>
-//                 Room page
-//             </h1>
-//             <ReactPlayer url={myStream} playing muted />
-//         </div>
-
-//     </>
-//   )
-// }
-
-// export default Room
-
-
-
-import { useEffect, useCallback, useState, useRef } from 'react';
-import { useSocket } from "../../Providers/Socket";
-import { usePeer } from "../../Providers/Peer";
+import { useEffect,useCallback, useState } from 'react'
+import {useSocket} from "../../Providers/Socket"
+import { usePeer } from "../../Providers/Peer"
+import ReactPlayer from 'react-player';
 
 function Room() {
-  const { socket } = useSocket();
-  const { peer, createOffer, createAnswer, setRemoteAns } = usePeer();
+    const {socket} = useSocket();
+    const {peer,createOffer,createAnswer,setRemoteAns,sendStream,remoteStream} = usePeer()
 
-  const [myStream, setMyStream] = useState(null);
-  const videoRef = useRef(null); // Ref for the video element
 
-  const newUserJoinedAfterOneUserJoined = useCallback(async (data) => {
-    const { email } = data;
-    console.log("new user joined", email);
-    const offer = await createOffer();
-    socket.emit('offerForCall-User', { email, offer });
-  }, [createOffer, socket]);
+    const [myStream,setMystream] = useState(null)
+    const [remoteEmailId,setRemoteEmailId] = useState()
 
-  const handelIncommingCall = useCallback(async (data) => {
-    const { from, offer } = data;
-    console.log("incoming call from", from, offer);
-    const ans = await createAnswer(offer);
-    socket.emit('call-accepted', { email: from, ans });
-  }, [createAnswer, socket]);
 
-  const handelCallAccepted = useCallback(async (data) => {
-    const { ans } = data;
-    console.log("call got accepted", ans);
-    await setRemoteAns(ans);
-  }, [setRemoteAns]);
+    const newUserJoinedAfterOneUserJoined = useCallback(async (data) => {
+        // console.log("recived data : ",JSON.stringify(data, null, 2))
+        const {email} = data
+        console.log("new user joined",email);   
+        const offer = await createOffer()
+        socket.emit('offerForCall-User',{email,offer})
+        setRemoteEmailId(email)
+    },[createOffer,socket])
 
-  const getUserMediaStream = useCallback(async () => {
-    try {
+    const handelIncommingCall = useCallback( async (data) => {
+      const {from,offer} = data;
+      console.log("incomming call from",from,offer)
+      const ans = await createAnswer(offer)
+      socket.emit('call-accepted',{email : from,ans})
+      setRemoteEmailId(from)
+
+      },[createAnswer,socket])
+
+    const handelCallAccepted = useCallback(async (data) => {
+      const {ans} = data;
+      console.log("call got accepted",ans)
+      await setRemoteAns(ans)
+      // sendStream(myStream)
+
+    } ,[setRemoteAns])
+
+    const getUserMediaStream = useCallback(async() => {
       const stream = await navigator.mediaDevices.getUserMedia({
         video: true,
-        audio: true,
-      });
-      setMyStream(stream);
-    } catch (error) {
-      console.error("Error accessing media devices:", error);
-    }
-  }, []);
+        audio: true
+      })
+      setMystream(stream)
+    }, [])
 
-  useEffect(() => {
-    if (myStream && videoRef.current) {
-      videoRef.current.srcObject = myStream; // Attach MediaStream to video element
-    }
-  }, [myStream]);
+    const handelNegotiation = useCallback(() => {
+        // console.log("negotition needed");
+        const loacalOffer = peer.localDescription;
+        socket.emit('offerForCall-User',{email : remoteEmailId,offer : loacalOffer})
+    }, [peer.localDescription, remoteEmailId, socket])
 
-  useEffect(() => {
-    socket.on('user-joined', newUserJoinedAfterOneUserJoined);
-    socket.on('incomming-call', handelIncommingCall);
-    socket.on('call-accepted', handelCallAccepted);
+    useEffect(() => {
+      socket.on('user-joined', newUserJoinedAfterOneUserJoined)
+      socket.on('incomming-call',handelIncommingCall)
+      socket.on('call-accepted',handelCallAccepted);
 
-    return () => {
-      socket.off('user-joined', newUserJoinedAfterOneUserJoined);
-      socket.off('incomming-call', handelIncommingCall);
-      socket.off('call-accepted', handelCallAccepted);
-    };
-  }, [socket, newUserJoinedAfterOneUserJoined, handelIncommingCall, handelCallAccepted]);
+      
+      return () => {
+        socket.off('user-joined', newUserJoinedAfterOneUserJoined) //securitu purpose and clean up also 
+        socket.off('incomming-call',handelIncommingCall)
+        socket.off('call-accepted',handelCallAccepted);
+      }
+    }, [socket,newUserJoinedAfterOneUserJoined,handelIncommingCall,handelCallAccepted])
+    
+    useEffect(() => {
+      peer.addEventListener('negotiationneeded', handelNegotiation)
+      return () => {
+        peer.removeEventListener('negotiationneeded', handelNegotiation)
+      }
+    }, [handelNegotiation, peer])
 
-  useEffect(() => {
-    getUserMediaStream();
-  }, [getUserMediaStream]);
+    useEffect(() => {
+      getUserMediaStream()
+    },[getUserMediaStream])
 
   return (
-    <div className='Room-page-container'>
-      <h1>Room page</h1>
-      <video
-        ref={videoRef}
-        autoPlay
-        playsInline
-        muted
-        style={{ width: '100%', height: 'auto', border: '1px solid black' }}
-      />
-    </div>
-  );
+    <>
+        <div className='Room-page-container'>
+            <h1>
+                Room page
+            </h1>
+            <h4>you are connected to {remoteEmailId}</h4>
+            <button onClick={(e) => sendStream(myStream)}>send my video</button>
+            <ReactPlayer url={myStream} playing muted />
+            <ReactPlayer url={remoteStream} playing muted />
+        </div>
+    </>
+  )
 }
 
-export default Room;
+export default Room
